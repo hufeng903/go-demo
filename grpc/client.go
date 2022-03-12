1dcd9f29f6f1f1964830ca2a99a8f06e537685f9
feng.hu <hu-feng@hotmail.com>
2022-03-12 11:38:59 +0800

    grpc demo

---
+++ grpc/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"demo/grpc/protobuf/user"
+	"encoding/json"
+	"fmt"
+	"google.golang.org/grpc"
+)
+
+func main(){
+	conn, err := grpc.Dial("127.0.0.1:8889", grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	client := user.NewUserServiceClient(conn)
+
+	req := &user.UserRequest{
+		Id: 1,
+	}
+
+	response, _ := client.GetUser(context.Background(), req)
+
+	resp,err := json.Marshal(response)
+
+	fmt.Printf("%s", resp)
+
+}
---
+++ grpc/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"demo/grpc/protobuf/user"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type UserServiceInterface interface {
+	GetUser(ctx context.Context, req *user.UserRequest) (*user.UserResponse, error)
+}
+
+type UserServiceStruct struct {
+
+}
+
+func NewUserService() UserServiceInterface {
+	return &UserServiceStruct{}
+}
+
+func (userService *UserServiceStruct) GetUser(ctx context.Context, req *user.UserRequest) (*user.UserResponse ,error) {
+	response := &user.UserResponse {
+		Id: req.Id,
+		Name: "Hello World",
+	}
+
+	return response,nil
+}
+
+func main()  {
+	l,err := net.Listen("tcp", "127.0.0.1:8889")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("listen on 127.0.0.1:8889")
+
+	grpcServer := grpc.NewServer()
+
+	var userService UserServiceInterface
+
+	userService = NewUserService()
+	user.RegisterUserServiceServer(grpcServer, userService)
+
+	err = grpcServer.Serve(l)
+
+	if err != nil {
+		println(err)
+	}
+
+
+}
